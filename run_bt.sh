2191c20ad4bfaee8988cd6e071f53ac7a554c33b
David Snyder <david.snyder.pi@tri.global>
2026-01-07 14:14:12 -0500

    Fix small error in run_BT.sh

--- run_bt.sh
+++ run_bt.sh
@@ -1,5 +1,5 @@
 # Test Run 
-python bradley_terry.py -it True -ng 100
+python bradley_terry.py -it 1 -ng 100
 
 # Run on progress-as-preference case
 python process_roboarena_data_for_bradley_terry.py
